6cd732e1d8f9432a726907f7b3ea32804e7053f7
Yosi <yosi121990@hotmail.es>
2023-03-03 08:52:33 +0000

    Ejer4 - distribuir funciones en 2 clases

--- Ej04/Fichero.cs
+++ Ej04/Fichero.cs
@@ -6,7 +6,9 @@ namespace Ej04
     {
         const string NOMBREFICH = "Datos notas.csv", FICHSALIDA = "salida.txt";
         static readonly double[] porcNotas = { 0.2, 0.3, 0.5 };
-        static List<string> lineas;
+        static List<string> lineas = new();
+
+
         public static bool ArchivoExiste() => File.Exists(NOMBREFICH);
 
         public static bool LeerFichero()
@@ -25,9 +27,10 @@ namespace Ej04
                 return false;
             }
         }
+
         public static bool RellenarFichero()
         {
-            CrearFichero(FICHSALIDA, true);
+            Funciones.CrearFichero(FICHSALIDA, true);
             try
             {
                 StreamWriter sw = new(FICHSALIDA);
@@ -54,66 +57,6 @@ namespace Ej04
                 Console.WriteLine($"escribir el fichero.\n\n\t{ex}");
                 Console.ReadKey();
                 return false;
-            }
-        }
-        public static bool CrearFichero(string path, bool sobreescribir = false)
-        {
-            try
-            {
-                bool crear = false;
-                if (File.Exists(path))
-                {
-                    if (sobreescribir)
-                    {
-                        Console.Write("\n\n\tEl archivo ya existe. \n\t\t¿Desea sobrescribirlo? S/N");
-                        if (Console.ReadKey().Key == ConsoleKey.S)
-                            crear = true;
-                    }
-                }
-                else
-                    crear = true;
-
-                if (crear)
-                {
-                    File.Create(path).Close();
-                    Cargar("\n\tCreando su fichero", "\n\n\t\t¡Archivo creado correctamente! :D", false, true);
-                }
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Continuar($"Fallo durante la creación del archivo\n\n\n{ex}");
-                return false;
-            }
-        }
-
-        public static void Cargar(string msg, string msgSalida = "", bool parada = true, bool limpiarPrevio = false)
-        {
-            if (limpiarPrevio)
-                Console.Clear();
-            Console.Write(msg);
-            for (int i = 0; i < 3; i++)
-            {
-                Thread.Sleep(10);
-                Console.Write(".");
-            }
-            Thread.Sleep(10);
-            Console.Write(msgSalida);
-            if (parada)
-                Continuar("");
-            else
-                Thread.Sleep(10);
-        }
-
-        public static void Continuar(string msg, bool parada = true, bool limpiarPrevio = false)
-        {
-            if (limpiarPrevio)
-                Console.Clear();
-            Console.Write(msg);
-            if (parada)
-            {
-                Console.Write("\n\n\tPulse una tecla para continuar... ");
-                Console.ReadKey();
             }
         }
     }
